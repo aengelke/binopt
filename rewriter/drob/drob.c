e370b8186ed73f32145d625f76d975903a65c4d1
Alexis Engelke <engelke@in.tum.de>
2020-02-19 10:29:38 +0100

    drob: Add initial support for Drob rewriter

---
+++ rewriter/drob/drob.c
@@ -0,0 +1,123 @@
+
+#include "binopt.h"
+#include "binopt-config.h"
+
+#include <drob.h>
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Drob bindings */
+
+const char* binopt_driver(void) {
+    return "Drob";
+}
+BinoptHandle binopt_init(void) {
+    drob_setup();
+    return NULL;
+}
+void binopt_fini(BinoptHandle handle) {
+    // No teardown, there may be multiple handles.
+    (void) handle;
+}
+
+static drob_param_type binopt_drob_map_ty(BinoptType ty) {
+    switch (ty) {
+    case BINOPT_TY_VOID: return DROB_PARAM_TYPE_VOID;
+    case BINOPT_TY_INT8: return DROB_PARAM_TYPE_INT8;
+    case BINOPT_TY_INT16: return DROB_PARAM_TYPE_INT16;
+    case BINOPT_TY_INT32: return DROB_PARAM_TYPE_INT32;
+    case BINOPT_TY_INT64: return DROB_PARAM_TYPE_INT64;
+    case BINOPT_TY_UINT8: return DROB_PARAM_TYPE_UINT8;
+    case BINOPT_TY_UINT16: return DROB_PARAM_TYPE_UINT16;
+    case BINOPT_TY_UINT32: return DROB_PARAM_TYPE_UINT32;
+    case BINOPT_TY_UINT64: return DROB_PARAM_TYPE_UINT64;
+    case BINOPT_TY_FLOAT: return DROB_PARAM_TYPE_FLOAT;
+    case BINOPT_TY_DOUBLE: return DROB_PARAM_TYPE_DOUBLE;
+    case BINOPT_TY_PTR: return DROB_PARAM_TYPE_PTR;
+    case BINOPT_TY_PTR_NOALIAS: return DROB_PARAM_TYPE_PTR;
+    default: return DROB_PARAM_TYPE_MAX;
+    }
+}
+
+BinoptFunc binopt_spec_create(BinoptCfgRef cfg) {
+    if (cfg->param_count > 6)
+        return cfg->func;
+
+    drob_set_logging(stderr, DROB_LOGLEVEL_DEBUG);
+
+    drob_param_type dty_ret = binopt_drob_map_ty(cfg->ret_ty);
+    drob_param_type dty_args[6] = {0};
+    for (size_t i = 0; i < cfg->param_count; i++)
+        dty_args[i] = binopt_drob_map_ty(cfg->params[i].ty);
+
+    drob_cfg* dcfg = drob_cfg_new(dty_ret, cfg->param_count, dty_args[0],
+                                  dty_args[1], dty_args[2], dty_args[3],
+                                  dty_args[4], dty_args[5]);
+
+    if (dcfg == NULL)
+        return cfg->func;
+
+    for (size_t i = 0; i < cfg->param_count; ++i) {
+        if (cfg->params[i].ty == BINOPT_TY_PTR_NOALIAS)
+            drob_cfg_set_ptr_flag(dcfg, i, DROB_PTR_FLAG_RESTRICT);
+        if (!cfg->params[i].const_val)
+            continue;
+        switch (cfg->params[i].ty) {
+        case BINOPT_TY_INT8:
+            drob_cfg_set_param_char(dcfg, i, *(char*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_INT16:
+            drob_cfg_set_param_short(dcfg, i, *(short*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_INT32:
+            drob_cfg_set_param_int(dcfg, i, *(int*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_INT64:
+            drob_cfg_set_param_longlong(dcfg, i, *(long long*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_UINT8:
+            drob_cfg_set_param_uchar(dcfg, i, *(unsigned char*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_UINT16:
+            drob_cfg_set_param_ushort(dcfg, i, *(unsigned short*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_UINT32:
+            drob_cfg_set_param_uint(dcfg, i, *(unsigned int*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_UINT64:
+            drob_cfg_set_param_ulonglong(dcfg, i, *(unsigned long long*) cfg->params[i].const_val);
+            break;
+        case BINOPT_TY_PTR:
+        case BINOPT_TY_PTR_NOALIAS:
+            drob_cfg_set_param_ptr(dcfg, i, *(const void**) cfg->params[i].const_val);
+            break;
+        default:
+            return cfg->func;
+        }
+    }
+    for (size_t i = 0; i < cfg->memrange_count; ++i) {
+        switch (cfg->memranges[i].flags) {
+        case BINOPT_MEM_CONST:
+        case BINOPT_MEM_NESTED_CONST:
+            drob_cfg_add_const_range(dcfg, cfg->memranges[i].base,
+                                     cfg->memranges[i].size);
+            break;
+        default:
+            // Do nothing.
+            break;
+        }
+    }
+
+    drob_cfg_dump(dcfg);
+    drob_cfg_set_error_handling(dcfg, DROB_ERROR_HANDLING_ABORT);
+    return (BinoptFunc) drob_optimize((drob_f) cfg->func, dcfg);
+}
+
+void binopt_spec_delete(BinoptHandle handle, BinoptFunc spec_func) {
+    drob_free((drob_f) spec_func);
+    (void) handle;
+}
