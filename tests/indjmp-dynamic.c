ca0fb2d0c7e55e55d85fb66ec1952c5ef03860cb
Alexis Engelke <engelke@in.tum.de>
2021-02-15 17:51:01 +0100

    tests: Add indirect jump with dynamic value

---
+++ tests/indjmp-dynamic.c
@@ -0,0 +1,18 @@
+#include "common.h"
+
+static int func(int a) {
+    __asm__ volatile(".section .data; 2: .8byte 1f; .previous jmp *2b(%%rip); 1:" :::);
+    return a;
+}
+
+int main(int argc, char** argv) {
+    BinoptHandle boh = test_init(argc, argv);
+    BinoptCfgRef bcfg = binopt_cfg_new(boh, (BinoptFunc) func);
+    binopt_cfg_type(bcfg, 1, BINOPT_TY_INT32, BINOPT_TY_INT32);
+    binopt_cfg_set_parami(bcfg, 0, 42);
+
+    int (* new_func)(int);
+    *((BinoptFunc*) &new_func) = binopt_spec_create(bcfg);
+    test_eq_i32(new_func(8), 8, 42);
+    test_fini();
+}
